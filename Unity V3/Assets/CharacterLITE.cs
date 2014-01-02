635bd74b5531224610ec53519698f402d6a3fa1c
Jordan Aupetit (Ordinateur Fixe) <aupetit.jordan@gmail.com>
2014-01-02 17:20:06 +0100

    Ajout du Rapport sur le jeu

--- Unity V3/Assets/CharacterLITE.cs
+++ Unity V3/Assets/CharacterLITE.cs
@@ -12,7 +12,8 @@ public class CharacterLITE : MonoBehaviour
 	private int nearEnnemi = 0;
 	private GameObject parentGO;
 	private AIPath scriptPath;
-	Stack<GameObject> mind;
+	private GameObject[] chests;
+	public Transform myTransformPosition;
 	
 	private class LifeCapsule
 	{
@@ -30,53 +31,24 @@ public class CharacterLITE : MonoBehaviour
 	
 	void Start ()
 	{
+		myTransformPosition = new GameObject().transform;
+		myTransformPosition.position = new Vector3(0.0f,0.0f,0.0f);
+
 		Debug.Log ("Ma Target est : " + target);
-		
-		/*if(target != null)
-			mind.Push(target);*/
 		
 		parentGO = gameObject.transform.parent.gameObject;
 		scriptPath = parentGO.GetComponent<AIPath>();
-		//scriptPath.target = target.transform;
 		
 		Debug.Log ("Mon parent est : " + parentGO.name + " Target : " + scriptPath.target);
 		
-		//movement = gameObject.GetComponent<AI_Comportement_V3>();
 		renderer.material.color = myColor;
 		
 		Vector3 pos = transform.position;
 		
 		LifeCapsule l = new LifeCapsule(pos);
 		lifeCapsule = l.lifeC;
-		
-		//field_of_view = FieldOfView.BuildFOV(this);
-		
-//		FieldOfView f = new FieldOfView(pos, this.gameObject);
-//		field_of_view = f.fov;
-		
-		//field_of_view = new GameObject();
-		//field_of_view.AddComponent("FieldOfView");
-	}
-	
-	void OnTriggerEnter (Collider col)
-    {
-		/*if(col.tag == "EnnemiTAG") {
-			Debug.Log ("Trig ENTER : " + col + " Tag : " + col.tag);
-			renderer.material.color = Color.red;
-			nearEnnemi++;
-			scriptPath.target = col.gameObject.transform;
-			//field_of_view.GetComponent<FieldOfView>().red();
-		}*/
-    }
-	
-	void OnTriggerExit (Collider col)
-    {
-		/*if(col.tag == "EnnemiTAG") {
-			nearEnnemi--;
-			
-			if(nearEnnemi <= 0)
-				renderer.material.color = Color.green;
-		}*/
+
+		chests = GameObject.FindGameObjectsWithTag ("ChestTAG");
 	}
 	
 	public bool Damage (float dmg) 
@@ -98,16 +70,22 @@ public class CharacterLITE : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		//Debug.Log("Je suis : " + gameObject + " Life : " + life);
+		if (scriptPath.target == null) {
+			foreach(GameObject chest in chests){
+				Debug.Log ("GO CHEST");
+
+				myTransformPosition.position = chest.transform.position;
+				scriptPath.target = myTransformPosition.transform;
+				scriptPath.canSearch = true;
+
+				break;
+			}
+		}
+
 		Vector3 pos = transform.position;
 		
 		lifeCapsule.transform.position = new Vector3(pos.x, pos.y + 2, pos.z);
 		lifeCapsule.transform.localScale = new Vector3(0.5f, life / 100 + 0.1f, 0.5f);
-		
-		//field_of_view.pos = pos;
-		//fov.pos = pos;
-		
-		//field_of_view.transform.position = pos;
 	}
 }
 
